548e6c0be191b50aa1a7344f99cf76824a3bfbe4
Maiker <gutierrezmaiker@gmail.com>
2025-09-18 18:34:48 -0600

    Add build command to package file

--- frontend/frontend-admin/src/layouts/BlogPreview.jsx
+++ frontend/frontend-admin/src/layouts/BlogPreview.jsx
@@ -2,9 +2,9 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getPost } from "../services/blog/get-post.js";
 import BlogHeader from "../components/blog/blog-preview/BlogHeader.jsx";
-import BlogBody from "../components/blog/blog-preview/BlogBody.jsx";
 import BlogComments from "../components/blog/blog-preview/BlogComments.jsx";
 import Loading from "../components/Loading.jsx";
+import BlogBody from "../components/blog/blog-preview/BlogBody.jsx";
 
 function BlogPreview() {
     const [title, setTitle] = useState();
